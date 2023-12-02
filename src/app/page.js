9040cb6dcd69123ff3f0db661b7d7620568cb8ba
Eloka Agu <elokaagu@Elokas-MacBook-Pro.local>
2023-12-02 15:41:00 +0000

    adding a header

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 "use client";
 import styled from "styled-components";
 import React from "react";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <Title>Culturin</Title>
       <p>Where inspiration, meets exploration</p>
     </div>
