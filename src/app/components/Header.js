6099a6ec13e38afb1f093f10750709c5b4cde585
Eloka <12972250+elokaagu@users.noreply.github.com>
2023-12-17 01:04:57 +0000

    styled components

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Search } from "styled-icons/boxicons-regular";
 import { ChevronDown } from "styled-icons/boxicons-regular";
@@ -10,11 +10,22 @@ import {
   DropdownMenu,
   Button,
   DropdownSection,
-  DropdownItem,
   cn,
 } from "@nextui-org/react";
 
+const options = ["United States", "Europe", "Africa", "Asia"];
+
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggling = () => setIsOpen(!isOpen);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const onOptionClicked = (value) => () => {
+    setSelectedOption(value);
+    setIsOpen(false);
+    console.log(selectedOption);
+  };
+
   return (
     <Head>
       <HeaderLeft>
@@ -35,29 +46,27 @@ export default function Header() {
         <ul>
           <li>
             <Link href="/search">
-              <Search size="20" />
+              <SearchIcon size="20" />
             </Link>
           </li>
+          {/* Countries <ChevronDown size="20" /> */}
           <li>
-            {/* Countries <ChevronDown size="20" /> */}
-            <Dropdown
-              backdrop="blur"
-              style={{
-                width: "250px",
-              }}
-            >
-              <DropdownTrigger>
-                <Button variant="flat" color="white" disableAnimation="true">
-                  Countries <ChevronDown size="20" />
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu aria-label="Static Actions">
-                <DropdownItem>United States</DropdownItem>
-                <DropdownItem>Europe</DropdownItem>
-                <DropdownItem>Africa</DropdownItem>
-                <DropdownItem>Asia</DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
+            <DropdownContainer>
+              <DropdownHeader onClick={toggling}>
+                Countries
+                <ChevronDown size="20" />
+              </DropdownHeader>
+              {isOpen && (
+                <DropdownListContainer>
+                  <DropdownList>
+                    <DropdownItem>USA</DropdownItem>
+                    <DropdownItem>Africa</DropdownItem>
+                    <DropdownItem>Asia</DropdownItem>
+                    <DropdownItem>Europe</DropdownItem>
+                  </DropdownList>
+                </DropdownListContainer>
+              )}
+            </DropdownContainer>
           </li>
           <li>News</li>
           <li>TV</li>
@@ -76,7 +85,7 @@ const Head = styled.div`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  height: 80px;
+  height: 60px;
   background: black;
   padding: 50px;
   flex: 1;
@@ -139,8 +148,11 @@ const SigninButton = styled.div`
   width: 100%;
   ${"" /* border: 1px solid white; */}
   padding: 10px;
-  padding-left: 20px;
-  padding-right: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-left: 10px;
+  padding-right: 10px;
   background-color: white;
   color: black;
   font-weight: 600;
@@ -149,5 +161,50 @@ const SigninButton = styled.div`
   &:hover {
     background: grey;
     transition: 0.3s ease-in-out;
+  }
+`;
+
+const SearchIcon = styled(Search)`
+  color: white;
+`;
+
+const DropdownHeader = styled.div`
+  /* width: 100%;
+  border: black;
+  display: flex;
+  flex-direction: row; */
+`;
+
+const DropdownContainer = styled("div")``;
+
+const DropdownList = styled("ul")`
+  margin: 30px;
+  margin-left: -10px;
+  color: black;
+  background: white;
+  z-index: 100;
+  border-radius: 10px;
+`;
+
+const DropdownItem = styled("li")`
+  list-style: none;
+  color: black;
+  width: 100%;
+`;
+
+const DropdownListContainer = styled.div`
+  position: absolute;
+  color: black;
+  width: 200px;
+  z-index: 100;
+
+  ul:hover {
+    list-style: none;
+    color: black;
+  }
+
+  ul li {
+    text-decoration: none;
+    color: black;
   }
 `;
