bc411f54bf9389fb66b3ade17a06a97f31d60400
Eloka <12972250+elokaagu@users.noreply.github.com>
2023-12-27 20:36:49 +0000

    added image feature

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -12,9 +12,16 @@ const imageStyle = {
 export default function Hero() {
   return (
     <AppBody>
-      <CardBody />
+      <CardBody>
+        <Image
+          src={"/ghosthouse.jpeg"}
+          alt="ghosthouse"
+          layout="fill"
+          objectFit="cover"
+        />
+      </CardBody>
       <CardText>
-        <h1>Beats Beyond Borders</h1>
+        <h1>Mexico City</h1>
         <CardAuthor>
           {/* <AvatarContainer>
             <Image
@@ -54,14 +61,21 @@ const AppBody = styled.div`
 
 const CardBody = styled.div`
   display: flex;
+  position: relative;
   flex-direction: column;
   justify-content: left;
   height: 300px;
-  padding: 20px;
+  /* padding: 20px; */
   border-radius: 8px;
   drop-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   background: #1a1a1a;
   cursor: pointer;
+
+  img {
+    border-radius: 8px;
+    width: 100%;
+    height: 100%;
+  }
 
   &:hover {
     background-color: #4444;
