9c3bbf9db8a99e4bbb81705ca6f636fbd8f283f6
Eloka Agu <elokaagu@Elokas-MacBook-Pro.local>
2024-01-12 10:30:08 +0000

    added back button

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,12 +4,35 @@ import React from "react";
 import styled from "styled-components";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Link from "next/link";
+import { device } from "../styles/breakpoints";
 
 export default function Posts() {
   return (
     <>
       <Header />
       <AppBody>
+        <Link href="/" passHref>
+          <BackLink>
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 12 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z"
+                fill="currentColor"
+                fillRule="evenodd"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            {"   "}
+            back
+          </BackLink>
+        </Link>
+
         <Title>
           <h1>Enugu, Nigeria</h1>
         </Title>
@@ -127,5 +150,26 @@ const Body = styled.div`
     font-size: 18px;
     padding-bottom: 36px;
     color: white;
+  }
+`;
+
+const BackLink = styled.a`
+  color: rgb(130, 143, 255);
+  padding-bottom: 20px;
+  text-decoration: none;
+  position: absolute;
+  left: 50px;
+  top: 200px;
+
+  :hover {
+    color: white;
+    cursor: pointer;
+    transition: all 0.5s ease-in-out;
+  }
+
+  @media ${device.laptop} {
+    position: absolute;
+    left: 35px;
+    top: 50px;
   }
 `;
