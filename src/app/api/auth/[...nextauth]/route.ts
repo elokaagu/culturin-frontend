45ec84c3a529f4cc6d5d12408e416417f4dfe8ad
Eloka Agu <eloka@satellitelabs.xyz>
2024-03-03 20:33:15 +0000

    local

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,14 +6,37 @@ const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-      authorization: {
-        params: {
-          prompt: "consent", // This ensures that the consent screen is displayed each time the user logs in.
-          access_type: "offline", // This requests a refresh token to be sent along with the access token.
-        },
-      },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account }: { user: any; account: any }) {
+      console.log("User", user);
+      console.log("Account", account);
+
+      if (account.provider === "google") {
+        const { name, email } = user;
+        try {
+          const res = await fetch("http://localhost:3000/api/user", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              name,
+              email,
+            }),
+          });
+
+          if (res.ok) {
+            return user;
+          }
+        } catch (error) {
+          console.log(error);
+          return false;
+        }
+      }
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
