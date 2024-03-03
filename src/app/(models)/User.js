a55b8b623c5703a13e17870753dbd39463717c18
Eloka Agu <eloka@satellitelabs.xyz>
2024-03-03 15:03:14 +0000

    mongoose

---
+++ src/app/(models)/User.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.Promise = global.Promise;
+
+const userSchema = new Schema(
+  {
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
