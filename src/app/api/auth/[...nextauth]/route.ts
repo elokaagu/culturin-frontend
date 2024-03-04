123b40dc51f20f66362f8e1d2c581ffebe8d43d1
Eloka Agu <eloka@satellitelabs.xyz>
2024-03-04 12:57:41 +0000

    changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,35 +8,35 @@ const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
-  callbacks: {
-    async signIn({ user, account }: { user: any; account: any }) {
-      console.log("User", user);
-      console.log("Account", account);
+  // callbacks: {
+  //   async signIn({ user, account }: { user: any; account: any }) {
+  //     console.log("User", user);
+  //     console.log("Account", account);
 
-      if (account.provider === "google") {
-        const { name, email } = user;
-        try {
-          const res = await fetch("http://localhost:3000/api/user", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              name,
-              email,
-            }),
-          });
+  //     if (account.provider === "google") {
+  //       const { name, email } = user;
+  //       try {
+  //         const res = await fetch("http://localhost:3000/api/user", {
+  //           method: "POST",
+  //           headers: {
+  //             "Content-Type": "application/json",
+  //           },
+  //           body: JSON.stringify({
+  //             name,
+  //             email,
+  //           }),
+  //         });
 
-          if (res.ok) {
-            return user;
-          }
-        } catch (error) {
-          console.log(error);
-          return false;
-        }
-      }
-    },
-  },
+  //         if (res.ok) {
+  //           return user;
+  //         }
+  //       } catch (error) {
+  //         console.log(error);
+  //         return false;
+  //       }
+  //     }
+  //   },
+  // },
 };
 
 const handler = NextAuth(authOptions);
