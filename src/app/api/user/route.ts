698188ac21f543c9a809c3fdf602ad745ba5b5ce
Eloka Agu <eloka@satellitelabs.xyz>
2024-03-08 21:06:13 +0000

    schema

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -3,8 +3,8 @@ import { connectMongoDB } from "../../../../lib/mongodb";
 import User from "../../models/User";
 
 export async function POST(request: any) {
-  const { name, email } = await request.json();
+  const { name, email, username } = await request.json();
   await connectMongoDB();
-  await User.create({ name, email });
+  await User.create({ name, email, username });
   return NextResponse.json({ message: "New User Registered" }, { status: 201 });
 }
---
+++ src/migrations/updateUserSchema.ts
@@ -0,0 +1,35 @@
+require("dotenv").config();
+import mongoose from "mongoose";
+import User from "../app/models/User";
+
+// Create a connection to the database
+mongoose.connect(
+  process.env.MONGODB_URI as string,
+  {
+    useUnifiedTopology: true,
+  } as any
+);
+
+// Function to update existing users
+async function updateExistingUsers() {
+  const users = await User.find({ username: { $exists: false } });
+
+  const userUpdates = users.map((user) => {
+    // Avoid overwriting if username already exists
+    if (!user.username) {
+      user.username = user.email.split("@")[0].replace(/\./g, "_");
+      return user.save();
+    }
+  });
+
+  await Promise.all(userUpdates);
+  console.log("All users have been updated.");
+}
+
+// Run the update
+updateExistingUsers()
+  .then(() => mongoose.disconnect())
+  .catch((error) => {
+    console.error("Error updating users:", error);
+    mongoose.disconnect();
+  });
