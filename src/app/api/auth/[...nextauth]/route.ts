9dcbd0cee9dd38408cf6db884e8cb4519387ac6d
Eloka Agu <eloka@satellitelabs.xyz>
2024-03-08 19:42:56 +0000

    fixes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -19,11 +19,16 @@ const authOptions = {
           const fullName = profile.name.split(" ");
           const firstName = fullName.shift(); // First name is the first part
           const lastName = fullName.join(" "); // Last name is the rest
+          const username = profile.email.split("@")[0].replace(/\./g, "");
 
           const existingUser = await User.findOne({ email: profile.email });
           if (!existingUser) {
             // Create a new user if they don't exist
-            await User.create({ name: profile.name, email: profile.email });
+            await User.create({
+              name: profile.name,
+              email: profile.email,
+              username,
+            });
           }
         } catch (error) {
           console.error("SignIn error:", error);
@@ -32,30 +37,21 @@ const authOptions = {
       }
       return true; // Return true to sign the user in
     },
-    session: async ({
-      session,
-      token,
-      user,
-    }: {
-      session: any;
-      token: any;
-      user: any;
-    }) => {
-      if (user) {
-        session.user.id = user.id; // Or user._id or token.sub based on the provider and database
+
+    async jwt(token: any, user: any) {
+      if (user?.username) {
+        token.username = user.username;
       }
+      return token;
+    },
+    session: async ({ session, token }: { session: any; token: any }) => {
+      session.user.username = token.username as string;
       return session;
     },
-  },
-  async jwt(token: any, user: any) {
-    if (user?._id) {
-      token.id = user._id;
-    }
-    return token;
   },
 };
 
 const handler = (req: any, res: any) =>
-  NextAuth(req, res, authOptions as NextAuthOptions);
+  NextAuth(req, res, authOptions as unknown as NextAuthOptions);
 
 export { handler as GET, handler as POST };
--- src/app/profile/[username].tsx
+++ src/app/profile/[username].tsx
@@ -1,14 +1,14 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import Header from "../../components/Header";
+import Header from "../components/Header";
 import Link from "next/link";
-import { device } from "../../styles/breakpoints";
-import ProfileCard from "../../components/ProfileCard";
+import { device } from "../styles/breakpoints";
+import ProfileCard from "../components/ProfileCard";
 import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme, GlobalStyles } from "../../styles/theme";
+import { lightTheme, darkTheme, GlobalStyles } from "../styles/theme";
 import { useSession } from "next-auth/react";
-import { fullBlog } from "../../../../lib/interface";
+import { fullBlog } from "../../../lib/interface";
 import { set } from "mongoose";
 
 export default function Profile({ params }: { params: { profileId: string } }) {
