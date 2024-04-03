d781aead8047bc5d1a9136d58fe7d71472a4c784
Eloka Agu <eloka@satellitelabs.xyz>
2024-04-03 23:34:29 +0100

    algolia part one

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -3,8 +3,10 @@ import React from "react";
 import styled from "styled-components";
 import { Search } from "styled-icons/boxicons-regular";
 import { useState } from "react";
-import algoliasearch from "algoliasearch/lite";
 import { useRouter, useSearchParams } from "next/navigation";
+import algoliasearch from "algoliasearch/lite";
+import { SearchBox } from "react-instantsearch";
+import { InstantSearchNext } from "react-instantsearch-nextjs";
 
 // Initialize Algolia search client
 const searchClient = algoliasearch(
@@ -13,49 +15,56 @@ const searchClient = algoliasearch(
 );
 
 // Initialize Algolia search index
-const searchIndex = searchClient.initIndex("YourIndexName");
 
 export default function SearchBar() {
-  const search = useSearchParams();
-  const [searchQuery, setSearchQuery] = useState(
-    search ? search.get("q") : null
-  );
-  const router = useRouter();
+  // const search = useSearchParams();
+  // const [searchQuery, setSearchQuery] = useState(
+  //   search ? search.get("q") : null
+  // );
+  // const router = useRouter();
 
-  const onSearch = async (event: any) => {
-    event.preventDefault();
-    try {
-      const result = await searchIndex.search(searchQuery || ""); // Provide a default value of an empty string for searchQuery
-      console.log(result); // You can process and use the search result as needed
-      // For example, redirect to a search results page with the query or display results directly
-      router.push(`/search?q=${searchQuery}`);
-    } catch (error) {
-      console.error("Algolia search error: ", error);
-    }
-  };
+  // const onSearch = async (event: any) => {
+  //   event.preventDefault();
+  //   try {
+  //     const result = await searchIndex.search(searchQuery || ""); // Provide a default value of an empty string for searchQuery
+  //     console.log(result); // You can process and use the search result as needed
+  //     // For example, redirect to a search results page with the query or display results directly
+  //     router.push(`/search?q=${searchQuery}`);
+  //   } catch (error) {
+  //     console.error("Algolia search error: ", error);
+  //   }
+  // };
 
-  function handleReset() {
-    setSearchQuery("");
-  }
+  // function handleReset() {
+  //   setSearchQuery("");
+  // }
 
   return (
     <Body>
-      <SearchBox>
+      <SearchContainer>
         <Search size="20" />
-        <SearchForm onSubmit={onSearch}>
-          <SearchInput
-            type="text"
-            name="search"
-            placeholder="Search"
-            value={searchQuery || ""}
-            onChange={(event) => setSearchQuery(event.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") handleReset();
-            }}
-            autoComplete="off"
-          />
-        </SearchForm>
-      </SearchBox>
+        <SearchField>
+          <InstantSearchNext
+            indexName="countries"
+            searchClient={searchClient}
+            routing
+          >
+            {/* <SearchBox searchAsYouType /> */}
+
+            <SearchInput
+              type="text"
+              name="search"
+              placeholder="Search"
+              // value={searchQuery || ""}
+              // onChange={(event) => setSearchQuery(event.target.value)}
+              // onKeyDown={(e) => {
+              //   if (e.key === "Enter") handleReset();
+              // }}
+              autoComplete="off"
+            />
+          </InstantSearchNext>
+        </SearchField>
+      </SearchContainer>
     </Body>
   );
 }
@@ -67,7 +76,7 @@ const Body = styled.div`
   justify-content: center;
 `;
 
-const SearchBox = styled.div`
+const SearchContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -112,4 +121,19 @@ const SearchForm = styled.form`
   display: flex;
   flex-direction: row;
   flex: 1;
+`;
+
+const SearchField = styled.div`
+  background: transparent;
+  margin-left: 10px;
+  outline: none;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex: 2;
+  border: none;
+  height: 100%;
+  color: white;
+  font-weight: 600;
+  font-size: 18px;
 `;
