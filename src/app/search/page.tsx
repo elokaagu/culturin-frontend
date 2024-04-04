5ce3610b86053f7242b8e8ff59a553f77e94ca41
Eloka Agu <eloka@satellitelabs.xyz>
2024-04-04 15:59:59 +0100

    carddata

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -9,6 +9,8 @@ import { simpleBlogCard } from "../../../lib/interface";
 import { device } from "../styles/breakpoints";
 import Link from "next/link";
 import { urlFor } from "../lib/sanity";
+import { videoCard } from "../../../lib/interface";
+import { Video } from "styled-icons/boxicons-regular";
 
 // const fetchArticlesByCategory = async (categoryName: string) => {
 //   const query = `
@@ -40,6 +42,29 @@ async function getData() {
   }
 }
 
+async function getVideoData() {
+  const query = `
+  *[_type== 'video'] | order(_createdAt desc) {
+      title,
+      uploader,
+      videoThumbnail,
+      description,
+      "currentSlug":slug.current,
+    }
+
+ `;
+
+  try {
+    const videoData = await client.fetch(query);
+    return videoData;
+  } catch (error) {
+    console.error("Failed to fetch data from Sanity:", error);
+    return [];
+  }
+}
+
+// Search Results
+
 export default function SearchResultsPage({
   searchParams,
 }: {
@@ -68,6 +93,10 @@ export default function SearchResultsPage({
         <Title>
           <h1>Search Results</h1>
         </Title>
+        <Subtitle>
+          <h3>Articles</h3>
+        </Subtitle>
+
         <ArticlesContainer>
           {data.map((cardData, index) => (
             <Card key={index}>
@@ -325,3 +354,103 @@ const AvatarContainer = styled.div`
   display: flex;
   margin-right: 6px;
 `;
+
+const VideoCard = styled.div`
+  padding-bottom: 20px;
+  padding-right: 20px;
+`;
+
+const VideoCardBody = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: left;
+  height: 200px;
+  width: 400px;
+  padding: 20px;
+  border-radius: 8px;
+  drop-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  background: #1a1a1a;
+  cursor: pointer;
+  box-shadow: 0px 6px 8px rgba(25, 50, 47, 0.08),
+    0px 4px 4px rgba(18, 71, 52, 0.02), 0px 1px 16px rgba(18, 71, 52, 0.03);
+
+  img {
+    border-radius: 8px;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+  }
+
+  &:hover {
+    background-color: #4444;
+    opacity: 0.4;
+    transform: scale(0.98);
+    transition: 0.3s ease-in-out;
+  }
+
+  @media ${device.laptop} {
+    height: 200px;
+  }
+
+  @media ${device.mobile} {
+    height: 200px;
+    width: 150px;
+  }
+`;
+
+const VideoCardText = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 20px;
+
+  color: ${(props) => props.theme.title};
+
+  h1 {
+    cursor: pointer;
+    font-size: 16px;
+
+    @media ${device.laptop} {
+      font-size: 16px;
+    }
+
+    @media ${device.mobile} {
+      font-size: 14px;
+    }
+  }
+
+  p {
+    cursor: pointer;
+    font-size: 14px;
+    -webkit-line-clamp: 2;
+
+    color: ${(props) => props.theme.subtitle};
+
+    @media ${device.laptop} {
+      font-size: 12px;
+      color: grey;
+    }
+
+    @media ${device.mobile} {
+      font-size: 12px;
+    }
+  }
+
+  span {
+    cursor: pointer;
+    font-size: 14px;
+
+    @media ${device.laptop} {
+      font-size: 12px;
+    }
+  }
+`;
+
+const VideoCardAuthor = styled.div`
+  display: flex;
+  pointer: cursor;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const VideoContainer = styled.div``;
