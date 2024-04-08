c74315c62a12d4526abb09938b37acf373af4567
Eloka Agu <eloka@satellitelabs.xyz>
2024-04-08 14:48:38 +0100

    user data

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -102,8 +102,10 @@ export default function ProfilePage() {
               {session?.user?.name?.split(" ")[0] + "'s" || "Your"} Profile
             </h1>
           </ProfileTitle>
-          <p>Name </p>
           <Row>
+            <ProfileCardBody>
+              <p>This is the profile section where you save</p>
+            </ProfileCardBody>
             {/* {savedArticles.map((article) => (
               <ProfileCard key={article._id} article={article} />
             ))} */}
@@ -140,4 +142,97 @@ const ProfileTitle = styled.div`
 
 const Row = styled.div`
   overflow: scroll;
+`;
+
+const ProfileCardBody = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: left;
+  height: 300px;
+  width: 300px;
+
+  border-radius: 8px;
+  drop-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  background: #1a1a1a;
+  cursor: pointer;
+  box-shadow: 0px 6px 8px rgba(25, 50, 47, 0.08),
+    0px 4px 4px rgba(18, 71, 52, 0.02), 0px 1px 16px rgba(18, 71, 52, 0.03);
+
+  img {
+    border-radius: 8px;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+  }
+
+  &:hover {
+    background-color: #4444;
+    opacity: 0.4;
+    transform: scale(0.98);
+    transition: 0.3s ease-in-out;
+  }
+
+  @media ${device.laptop} {
+    height: 200px;
+  }
+
+  @media ${device.mobile} {
+    height: 200px;
+    width: 150px;
+  }
+`;
+
+const ProviderCardText = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 20px;
+
+  color: ${(props) => props.theme.title};
+
+  h1 {
+    cursor: pointer;
+    font-size: 16px;
+
+    @media ${device.laptop} {
+      font-size: 16px;
+    }
+
+    @media ${device.mobile} {
+      font-size: 14px;
+    }
+  }
+
+  p {
+    cursor: pointer;
+    font-size: 14px;
+    -webkit-line-clamp: 2;
+
+    color: ${(props) => props.theme.subtitle};
+
+    @media ${device.laptop} {
+      font-size: 12px;
+      color: grey;
+    }
+
+    @media ${device.mobile} {
+      font-size: 12px;
+    }
+  }
+
+  span {
+    cursor: pointer;
+    font-size: 14px;
+
+    @media ${device.laptop} {
+      font-size: 12px;
+    }
+  }
+`;
+
+const ProviderCardAuthor = styled.div`
+  display: flex;
+  pointer: cursor;
+  flex-direction: row;
+  align-items: center;
 `;
